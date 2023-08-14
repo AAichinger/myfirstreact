464a968de12509059f5a59d3c4645e2df7b14a1c
Andreas Aichinger <aichingerandreas@hotmail.com>
2023-08-14 14:18:34 +0200

    Card and Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Typography } from '@mui/material'
-import { Button } from '@mui/material'
+import MediaCard from './assets/card.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-          <Typography variant="h1">Hello World</Typography>
-          
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Typography
+      variant="h1"
+      component="h3"
+      gutterBottom
+      sx={{ color: 'red' }}
+    >Apfelstrudel</Typography>
+    <Typography
+      variant="h1"
+      component="h3"
+      gutterBottom
+      sx={{ color: 'cream' }}
+    >Apfelstrudel</Typography>
+    <Typography
+      variant="h1"
+      component="h3"
+      sx={{ color: 'red' }}
+    >Apfelstrudel</Typography>
+    {/*<MediaCard/>*/}
     </>
+
+
   )
 }
+
 
 
 export default App
